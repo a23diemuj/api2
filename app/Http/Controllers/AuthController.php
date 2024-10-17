e8a929653403a935bb84078d77b11990dbce90f5
a23diemujper <a23diemujper@inspedralbes.cat>
2024-10-17 13:10:03 +0200

    version2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     public function register(Request $request){
