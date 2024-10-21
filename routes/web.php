4ea84154b7137b232edf63fa4b344844318575f4
a23diemujper <a23diemujper@inspedralbes.cat>
2024-10-21 09:11:59 +0200

    ultimaversion

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::post("/register", [AuthController::class,"register"]);
-Route::post("/login", [AuthController::class,"login"]);
